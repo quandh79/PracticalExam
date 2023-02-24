5752feec1e66f5d4d5a9eb08c14553ccfce069b7
quandh79 <dohongquantc@gmail.com>
2023-02-24 20:54:14 +0700

    1

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class StudentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "name"=>$this->faker->name,
+            "age"=>random_int(10,20),
+            "address"=>$this->faker->address,
+            "telephone"=>$this->faker->phoneNumber,
+        ];
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::prefix('/')->group(function() {
+    Route::get('',[HomeController::class,'listStudents'])->name('listStudents');
+    Route::get('create',[HomeController::class,'createStudents'])->name('createStudents');
+    Route::post('create',[HomeController::class,'addStudents']);
+    Route::get('update/{id}',[HomeController::class,'getUpdateStudents'])->name('Students.getUpdate');
+    Route::post('update/{id}',[HomeController::class,'updateStudents'])->name('Students.update');
+    Route::get('delete/{id}', [HomeController::class, 'deleteStudents'])->name('Students.delete');
+});
